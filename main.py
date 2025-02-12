ee5e07c19e11444d89a766d85ce1cf5579ebd4c6
Matcbyrne <matc.byrne@gmail.com>
2025-02-12 00:19:18 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__, template_folder="templates")
 
 # Load API Key
-GROQ_API_KEY = os.getenv("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")
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 if not GROQ_API_KEY:
     raise ValueError("Error: Please set your GROQ_API_KEY environment variable.")
 
